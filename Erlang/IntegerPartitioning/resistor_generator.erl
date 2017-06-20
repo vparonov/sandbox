3f94e2e93ea3f4f6d59bc921194996b0a2589dfb
Vangel Paronov <v_paronov@applss.com>
2017-06-20 15:08:24 +0300

    Fixes

--- Erlang/IntegerPartitioning/resistor_generator.erl
+++ Erlang/IntegerPartitioning/resistor_generator.erl
@@ -3,101 +3,15 @@
 %c("resistor_generator.erl").
 %resistore_generator:test_g(3).
 -module(resistor_generator).
--export([test/0,test_g/1,test_r/1,toString/1]).
+-export([test_g/2,test_r/2, takeN/2,dump_values/2,fillUpTo/3]).
 -include_lib("stdlib/include/ms_transform.hrl").
 
 -record(fraction, {nominator, denominator}).
 
 fraction(A,B) -> #fraction{nominator=A,denominator=B}.
 
-toString(Fraction) ->
-	io_lib:format("~w/~w", [trunc(Fraction#fraction.nominator), trunc(Fraction#fraction.denominator)]).
-
-gcd(A, B) when A =:= B -> A;
-gcd(A, B) when A > B ->
-	gcd(A-B, B);
-gcd(A, B)  -> 
-	gcd(A, B-A).	
-	
-
-lcd(A, B) -> 
-	(A  / gcd(A, B)) * B.	
-
-
-add(FA, FB) ->
-	D = lcd(FA#fraction.nominator, FB#fraction.denominator),
-	N = FA#fraction.nominator * (D / FA#fraction.denominator) + FB#fraction.nominator * (D / FB#fraction.denominator),
-	fraction(N, D).
-
-reduce(F) ->
-	G = gcd(F#fraction.nominator, F#fraction.denominator), 
-	fraction(F#fraction.nominator / G, F#fraction.denominator / G).
-
-chunks(F) ->
-	F#fraction.nominator + F#fraction.denominator.
-
-invert(F) ->
-	fraction(F#fraction.denominator, F#fraction.nominator).
-
 toFp(F) ->
 	F#fraction.nominator / F#fraction.denominator.	
-
-g(S,_,A,NN) when S >= NN -> 
-	A;
-g(S,N,A,NN) when N =< NN ->
-	I = N + 1,
-	g(S+I,I,[I|A], NN).
-
-%
-% tests
-%
-test_fraction() ->
-	F = fraction(1,2),
-	io:fwrite("~s~n", [toString(F)]).
-
-test_toString() ->
-	A = #fraction{nominator=1, denominator=2},	
-	io:fwrite("fraction as string ~s~n",[toString(A)]).
-	
-
-test_gcd() ->
-	G = gcd(54,24),
-	io:fwrite("~p~n", [G]).
-
-test_lcd() ->
-	L = lcd(2, 4), 
-	io:fwrite("~p~n", [L]).
-
-test_add() ->
-	A = fraction(1,3),
-	B = fraction(1,3),
-	C = reduce(add(A, B)), 
-	D = reduce(add(C, C)), 
-	io:fwrite("~s + ~s = ~s, ~s ~n",[toString(A),toString(B),toString(C), toString(D)]).
-
-test_chunks() ->
-	A = fraction(3,4),
-	io:fwrite("~s = ~p chunks~n", [toString(A), chunks(A)]).
-
-test_invert() ->
-	A = fraction(3,4), 
-	io:fwrite("~s => inverted = ~s ~n", [toString(A), toString(invert(A))]).	
-
-test_toFp() ->
-	A = fraction(3,4), 
-	io:fwrite("~s => float = ~p ~n", [toString(A), toFp(A)]).	
-
-test() ->
-	test_toString(),
-	test_gcd(),
-	test_lcd(),
-	test_fraction(),
-	test_add(),
-	test_chunks(),
-	test_invert(),
-	test_toFp(),
-	g(0,-1,[],5).
-	
 
 mp(N) ->
     lists:foreach(fun (X) -> put(X, undefined) end,
@@ -125,28 +39,34 @@ prepend(X, [H | T], R) -> prepend(X, T, [[X | H] | R]).
 toFractions(L) ->
 	lists:map(fun(LI) -> fraction(1, LI) end, L).
 
-tranformF()->
+tranformF(_)->
 	% function that transforms
 	% list [a, b, c, d] into tuple {1/(1/a + 1/b + 1/c + 1/d), lenghOfl, [a, b, c, d]}
+	%Coef = Decimals * 10, 
 	fun(LI) -> 
 		{
-			1.0 / lists:foldl(fun(Item, Agg) -> Agg + toFp(Item) end, 0, toFractions(LI)), 
-			{lchunks(LI), LI}
+			%round(Coef *
+				(1.0 / lists:foldl(fun(Item, Agg) -> Agg + toFp(Item) end, 0, toFractions(LI)))
+				%) / Coef
+			,{lchunks(LI), LI}
 		} 
 	end.
 
 lchunks(L) ->
 		lists:foldl(fun(I, Agg) -> Agg + I end, 0, L).
 
-generateResistorNetworks(N) ->
+generateResistorNetworks(N, Decimals) ->
 	%generate all partitions from 1 to N
 	L = lists:map(fun(I) -> mp(I) end,lists:seq(1, N)),
 	
 	%transform every parition into a circuit from parallel connected resistors
-	All = lists:flatten(lists:map(fun(LI) -> lists:map(tranformF(), LI) end, L)),
+	All = lists:flatten(lists:map(fun(LI) -> lists:map(tranformF(Decimals), LI) end, L)),
 
 	%sort all circuits by value and number of chunks
-	Sorted = lists:sort(fun({A,{Chunks1, _}}, {B,{Chunks2,_}}) -> ((A > B) orelse (A =:= B andalso Chunks1 > Chunks2)) end, All),
+	Sorted = lists:sort(
+		fun({A,{Chunks1, _}}, {B,{Chunks2,_}}) -> 
+			((A > B) orelse (A =:= B andalso Chunks1 > Chunks2)) 
+		end, All),
 	maps:to_list(maps:from_list(Sorted)).
 
 saveGeneratedResistorNetworks(FileName, ResistorNetworks) ->
@@ -161,27 +81,61 @@ openRNF(FileName) ->
 closeRNF(Ref) ->
 	dets:close(Ref).
 
+takeN(N, L) -> takeN(N, L, []).
+takeN(_, [], R)  -> 
+	lists:reverse(R);
+takeN(0, _,  R)  -> 
+	lists:reverse(R);
+takeN(N, [H|T],  R)  -> 
+	takeN(N-1, T, [H|R]).
+	
 
-test_g(N) ->
-	saveGeneratedResistorNetworks("data.dat", generateResistorNetworks(N)).
+fillUpTo(N,Filler,List) -> fillUpTo(N,Filler,List,[]).
 
-test_r(Inputs) ->
+fillUpTo(0,_,_,Result)          -> lists:reverse(Result) ;
+fillUpTo(N,Filler,[],Result)    -> fillUpTo(N-1,Filler,[],[Filler|Result]);
+fillUpTo(N,Filler,[H|T],Result) -> fillUpTo(N-1,Filler,T ,[H|Result]).
+
+
+
+
+test_g(N, Decimals) ->
+	saveGeneratedResistorNetworks("data.dat", generateResistorNetworks(N, Decimals)).
+
+test_r(Inputs, Tolerance) ->
 	Table = openRNF("data.dat"),
-	RetVal = lists:map(fun(D) ->
-		Tol = D * 0.10, % search within 10% tolerance 	
-		Selection = 
-			dets:select
-			(
-				Table, 
-				ets:fun2ms(
-						fun({Value, {Chunks, L}}) when abs(Value - D) =< Tol-> {round(1000000 * abs(Value - D)), Value, Chunks, L} end)
-			),
-		SortedSelection = lists:sort(fun({A,_,Ca,_}, {B,_,Cb,_}) -> A < B orelse (A =:= B andalso Ca < Cb) end, Selection),
-		{Top10FromSelection,_} = lists:split(10, SortedSelection), 
-		Top10FromSelection
-	end, Inputs),
+	TolCoef = Tolerance / 100.0, 
+	RetVal = lists:map(
+		fun(D) ->
+			Tol = D * TolCoef, % search within 10% tolerance 	
+			Selection = 
+				dets:select
+				(
+					Table, 
+					ets:fun2ms(
+							fun({Value, {Chunks, L}}) 
+								when abs(Value - D) =< Tol -> 
+									{round(1000000 * abs(Value - D)), Value, Chunks, L} 
+							end)
+				),
+			SortedSelection = lists:sort(fun({A,_,Ca,_}, {B,_,Cb,_}) -> A < B orelse (A =:= B andalso Ca < Cb) end, Selection),
+			takeN(10, SortedSelection)
+		end, Inputs),
 	closeRNF(Table),
 	RetVal.
 
+dump_values(N, OutFileName) ->
+	Table = openRNF("data.dat"),
+	All = dets:traverse(Table, fun(X) -> {continue, X} end),
+	Sorted = lists:sort(fun({A,{_, _}}, {B,{_,_}}) -> A < B end, All), 
+	Result = lists:map(
+		fun({FloatValue,{Chunks, List}}) ->
+			io_lib:format("~w~c~w~c~w~n", [FloatValue, 9,Chunks, 9,fillUpTo(N,0,List)])
+		end, Sorted),
+	closeRNF(Table),
+	file:write_file(OutFileName,Result).
+
+
+		
 
 
